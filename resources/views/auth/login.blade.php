9bcb9ecb82b2b0be7d853906fa86f62d2e32ac54
Ariful Islam Polash <arifulislampolash.25.fswad@gmail.com>
2022-09-05 20:22:40 +0600

    social button added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,13 +41,17 @@
 
                         <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
+                                <a href="" class="btn btn-success">Facebook</a>
+                                <a href="" class="btn btn-success">Google</a>
+                                <a href="" class="btn btn-success">Github</a>
+
+                                {{-- <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
-                                </div>
+                                </div> --}}
                             </div>
                         </div>
 
